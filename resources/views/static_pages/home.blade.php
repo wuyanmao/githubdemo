ffa6dd9f532cc8d3881b971b04b1e797e3218601
wuyanmao <1218808495@qq.com>
2019-05-05 17:49:12 +0800

    基础页面

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','主页')
+@section('content')
+	<h1>home</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,64 +11,72 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
 
-});
-Route::get('/user',function(){
-    return view('welcome');
-});
-Route::any('/home',function(){
-    //any语法;
-    echo 'sajidjsaijdsa';
-});
-Route::match(['get','post'],'/test1',function() {
-    echo '当前的位置/test1';
-    });
-
-//可选参数
-Route::any('test2/{id}',function($id = null){
-    echo "当前的用户id是"  . $id;
-});
-
-//通过?传参 要在浏览器当中写入id=参数值
-Route::any('/user1',function(){
-    echo "当前的用户名" . $_GET['id'];
-});
-
-//路由别名
-Route::any('/wusadaaaaaaaaaaaaaaaaaadadwadf/awfawfjhaudhuiwahauf',function(){
-    echo 'sdsad';
-}) -> name('wus');
-
-//控制器路由
-Route::any('/wuwuwu','Admin\IndexController@index');
-Route::any('/wuwuwu1','Home\IndexController@index');
+Route::get('/','StaicPagesController@home');
+Route::get('/help','StaicPagesController@help');
+Route::get('/about','StaicPagesController@about');
 
 
 
-Route::group(['prefix' =>'home/test'],function(){
-	Route::any('add','TestController@add');
-	Route::any('del','TestController@del');
-	Route::any('update','TestController@update');
-	Route::any('select','TestController@select');
-	Route::any('test2','Testcontroller@test3');//第一个是路径  控制器@方法
 
-});
-Route::any('getQuestion','QuestionControllers@getQuestion');
-Route::any('home/test/test8','QuestionControllers@test8');
-Route::any('home/test/test9','QuestionControllers@test9');
-Route::any('home/test/test11','QuestionControllers@test11');
-
-Route::any('home/wu','QuestionControllers@wu');
-//pagein页面
-Route::any('home/test/page','QuestionControllers@pagein');
-//关于session
-Route::get('home/test/ses','QuestionControllers@ses');
-//关于cache
-Route::get('home/test/cac','QuestionControllers@cac');
-
-Route::get('test1/1','QuestionControllers@ceshi');
+//Route::get('/', function () {
+//    return view('welcome');
+//
+//});
+//Route::get('/user',function(){
+//    return view('welcome');
+//});
+//Route::any('/home',function(){
+//    //any语法;
+//    echo 'sajidjsaijdsa';
+//});
+//Route::match(['get','post'],'/test1',function() {
+//    echo '当前的位置/test1';
+//    });
+//
+////可选参数
+//Route::any('test2/{id}',function($id = null){
+//    echo "当前的用户id是"  . $id;
+//});
+//
+////通过?传参 要在浏览器当中写入id=参数值
+//Route::any('/user1',function(){
+//    echo "当前的用户名" . $_GET['id'];
+//});
+//
+////路由别名
+//Route::any('/wusadaaaaaaaaaaaaaaaaaadadwadf/awfawfjhaudhuiwahauf',function(){
+//    echo 'sdsad';
+//}) -> name('wus');
+//
+////控制器路由
+//Route::any('/wuwuwu','Admin\IndexController@index');
+//Route::any('/wuwuwu1','Home\IndexController@index');
+//
+//
+//
+//Route::group(['prefix' =>'home/test'],function(){
+//	Route::any('add','TestController@add');
+//	Route::any('del','TestController@del');
+//	Route::any('update','TestController@update');
+//	Route::any('select','TestController@select');
+//	Route::any('test2','Testcontroller@test3');//第一个是路径  控制器@方法
+//
+//});
+//Route::any('getQuestion','QuestionControllers@getQuestion');
+//Route::any('home/test/test8','QuestionControllers@test8');
+//Route::any('home/test/test9','QuestionControllers@test9');
+//Route::any('home/test/test11','QuestionControllers@test11');
+//
+//Route::any('home/wu','QuestionControllers@wu');
+////pagein页面
+//Route::any('home/test/page','QuestionControllers@pagein');
+////关于session
+//Route::get('home/test/ses','QuestionControllers@ses');
+////关于cache
+//Route::get('home/test/cac','QuestionControllers@cac');
+//
+//Route::get('test1/1','QuestionControllers@ceshi');
 
 
 
