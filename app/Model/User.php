d9a81044954e37b03a340a6d42b8f0bd7879a40a
wuyanmao <1218808495@qq.com>
2019-05-06 19:25:16 +0800

    moxing

---
+++ app/Model/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+	use Notifiable;
+
+	protected $table = 'users';
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+
+	protected $fillable = [
+		'name', 'email', 'password',
+	];
+
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+}
