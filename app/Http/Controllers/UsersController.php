9fd2f9846dcff56328b19ea4e4883110077666a5
wuyanmao <1218808495@qq.com>
2019-05-07 16:28:45 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,4 +13,13 @@ class UsersController extends Controller
 	public function show(User $user){
 		return view('users.show',compact('user'));
 	}
+	public function store (Request $request){
+		$this->verifyField($request->all(),[
+			'name' => 'required|string',
+			'email' => 'required|email|unique:users|max:255',
+			'password' => 'required|confirmed|min:6'
+		]);
+
+	}
+
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
         </div>
         <div class="card-body">
             <form method="POST" action="{{ route('users.store') }}">
+                {{ csrf_field() }}
                 <div class="form-group">
                     <label for="name">名称：</label>
                     <input type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -27,7 +28,6 @@
                     <label for="password_confirmation">确认密码：</label>
                     <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
                 </div>
-
                 <button type="submit" class="btn btn-primary">注册</button>
             </form>
         </div>
